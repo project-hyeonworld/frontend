4fc0ef49d15edb8f8f83bfd0244467d2b24fa90a
hyeonwoody <hyeonwoody@gmail.com>
2024-11-15 21:07:24 +0900

    git : Fix .gitignore and add `SpecialConfig`

---
+++ src/configuration/special/SpecialConfig.ts
@@ -0,0 +1,12 @@
+export class Special {
+    adminId : number;
+    adminName : string;
+    monitor : number;
+    constructor() {
+        this.adminId = {MY_SPECIAL};
+        this.adminName = "{MY_ADMIN}";
+        this.monitor = {MY_MONITOR};
+    }
+}
+
+export default {Special};
